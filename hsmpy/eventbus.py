c0d12f7d2d7f96d7335aa9f6117387d689fdc473
bgr-ubuntu-vm <bgr.gyk@gmail.com>
2013-07-05 06:26:43 -0700

    EventBus event queuing functionality

--- hsmpy/eventbus.py
+++ hsmpy/eventbus.py
@@ -1,8 +1,18 @@
+class Event(object):
+    def __init__(self, data):
+        self.data = data
+
+
 class EventBus(object):
     def __init__(self):
         self.listeners = {}
+        self.queue = []
+        self.dispatch_in_progress = False
 
     def register(self, event_type, callback):
+        if not issubclass(event_type, Event):
+            raise TypeError("Must be a subclass of Event")
+
         if event_type not in self.listeners:
             self.listeners[event_type] = set()
         self.listeners[event_type].add(callback)
@@ -22,6 +32,30 @@ class EventBus(object):
         if len(group) == 0:
             del self.listeners[event_type]
 
-    def dispatch(self, event, aux=None):
-        callbacks = [cb for cb in self.listeners.get(event, [])]
-        [cb(event, aux) for cb in callbacks]
+    def dispatch(self, event):
+        if not isinstance(event, Event):
+            raise TypeError("Must subclass Event")
+
+        # TODO: check for infinite dispatch loops
+
+        if not self.dispatch_in_progress:
+            # set lock for upcoming calls to 'dispatch' method
+            self.dispatch_in_progress = True
+
+            # gather all callbacks registered for initial event
+            callbacks = [cb for cb in self.listeners.get(event.__class__, [])]
+
+            while True:  # more events might get queued up while dispatching
+                [cb(event) for cb in callbacks]  # perform gathered calls
+                if not self.queue:  # check if new events have been queued up
+                    break
+                # events were queued, gather new callbacks for queued events
+                callbacks = [cb for evt in self.queue
+                             for cb in self.listeners.get(evt.__class__, [])]
+                self.queue = []
+
+            # unlock
+            self.dispatch_in_progress = False
+
+        else:  # dispatching currently in progress, add event to queue
+            self.queue += [event]
--- tests/test_eventbus.py
+++ tests/test_eventbus.py
@@ -1,13 +1,16 @@
 import pytest
-from hsmpy.eventbus import EventBus
+from hsmpy.eventbus import EventBus, Event
 
 
 class Dummy(object):
     def __init__(self):
         self.x = 1
 
-    def increment(self, *_):
-        self.x += 1
+    def increment(self, evt):
+        self.x += evt.data
+
+class PingEvent(Event): pass
+class AnotherEvent(Event): pass
 
 
 class Test_single_listener(object):
@@ -17,19 +20,21 @@ class Test_single_listener(object):
         self.obj = Dummy()
 
     def test_can_register(self):
-        self.eb.register('hey', self.obj.increment)
+        self.eb.register(PingEvent, self.obj.increment)
         assert self.obj.x == 1  # unchanged
 
     def test_can_fire(self):
-        self.eb.dispatch('hey')
+        self.eb.dispatch(PingEvent(1))
         assert self.obj.x == 2
-        self.eb.dispatch('hey')
-        assert self.obj.x == 3
+        self.eb.dispatch(AnotherEvent(1))
+        assert self.obj.x == 2  # unchanged
+        self.eb.dispatch(PingEvent(4))
+        assert self.obj.x == 6
 
     def test_can_unregister(self):
-        self.eb.unregister('hey', self.obj.increment)
-        self.eb.dispatch('hey')
-        assert self.obj.x == 3  # unchanged
+        self.eb.unregister(PingEvent, self.obj.increment)
+        self.eb.dispatch(PingEvent(2))
+        assert self.obj.x == 6  # unchanged
 
 
 class Test_multiple_listeners(object):
@@ -39,44 +44,46 @@ class Test_multiple_listeners(object):
         self.deaf = Dummy()
         self.hey = Dummy()
         self.you = Dummy()
-        self.eb.register('hey', self.hey.increment)
-        self.eb.register('you', self.you.increment)
+        self.eb.register(PingEvent, self.hey.increment)
+        self.eb.register(AnotherEvent, self.you.increment)
         assert self.deaf.x == 1
         assert self.hey.x == 1
         assert self.you.x == 1
 
     def test_firing_impacts_only_interested_listener(self):
-        self.eb.dispatch('hey')
-        assert self.deaf.x == 1  # unchanged
-        assert self.hey.x == 2
-        assert self.you.x == 1  # unchanged
-        self.eb.dispatch('hey')
-        assert self.deaf.x == 1  # unchanged
+        self.eb.dispatch(PingEvent(1))
+        assert self.deaf.x == 1
+        assert self.hey.x == 2  # changed
+        assert self.you.x == 1
+        self.eb.dispatch(PingEvent(1))
+        assert self.deaf.x == 1
+        assert self.hey.x == 3  # changed
+        assert self.you.x == 1
+        self.eb.dispatch(AnotherEvent(1))
+        assert self.deaf.x == 1
         assert self.hey.x == 3
-        assert self.you.x == 1  # unchanged
-        self.eb.dispatch('you')
-        assert self.deaf.x == 1  # unchanged
-        assert self.hey.x == 3
-        assert self.you.x == 2  # unchanged
+        assert self.you.x == 2  # changed
 
     def test_others_work_after_one_is_unregistered(self):
-        self.eb.unregister('hey', self.hey.increment)
-        self.eb.dispatch('hey')
-        assert self.deaf.x == 1  # unchanged
-        assert self.hey.x == 3  # unchanged
-        assert self.you.x == 2  # unchanged
-        self.eb.dispatch('you')
-        assert self.deaf.x == 1  # unchanged
-        assert self.hey.x == 3  # unchanged
-        assert self.you.x == 3  # unchanged
+        self.eb.unregister(PingEvent, self.hey.increment)
+        self.eb.dispatch(PingEvent(2))
+        # all unchanged
+        assert self.deaf.x == 1
+        assert self.hey.x == 3
+        assert self.you.x == 2
+        self.eb.dispatch(AnotherEvent(2))
+        assert self.deaf.x == 1
+        assert self.hey.x == 3
+        assert self.you.x == 4  # changed
 
     def test_can_unregister_last_listener(self):
-        self.eb.unregister('you', self.you.increment)
-        self.eb.dispatch('hey')
-        self.eb.dispatch('you')
-        assert self.deaf.x == 1  # unchanged
-        assert self.hey.x == 3  # unchanged
-        assert self.you.x == 3  # unchanged
+        self.eb.unregister(AnotherEvent, self.you.increment)
+        self.eb.dispatch(PingEvent(3))
+        self.eb.dispatch(AnotherEvent(3))
+        # all unchanged
+        assert self.deaf.x == 1
+        assert self.hey.x == 3
+        assert self.you.x == 4
 
 
 class Test_raising_errors(object):
@@ -87,20 +94,58 @@ class Test_raising_errors(object):
         self.hey = Dummy()
         self.you = Dummy()
 
+    def test_raise_when_event_is_of_wrong_type(self):
+        with pytest.raises(TypeError):
+            self.eb.register(EventBus, self.hey.increment)
+
     def test_raise_when_trying_to_unregister_before_registering(self):
         with pytest.raises(LookupError):
-            self.eb.unregister('hey', self.hey.increment)
+            self.eb.unregister(PingEvent, self.hey.increment)
 
     def test_raise_when_unregistering_wrong_listener(self):
-        self.eb.register('hey', self.hey.increment)
+        self.eb.register(PingEvent, self.hey.increment)
         with pytest.raises(LookupError):
-            self.eb.unregister('hey', self.you.increment)
+            self.eb.unregister(PingEvent, self.you.increment)
 
     def test_raise_when_unregistering_from_wrong_event_type(self):
         with pytest.raises(LookupError):
-            self.eb.unregister('you', self.hey.increment)
+            self.eb.unregister(AnotherEvent, self.hey.increment)
 
     def test_raise_when_already_unregistered(self):
-        self.eb.unregister('hey', self.hey.increment)
+        self.eb.unregister(PingEvent, self.hey.increment)
         with pytest.raises(LookupError):
-            self.eb.unregister('hey', self.hey.increment)
+            self.eb.unregister(PingEvent, self.hey.increment)
+
+
+class Test_event_queuing(object):
+    def setup_class(self):
+        self.eb = EventBus()
+
+        self.x = 1
+        self.first_visited = 'not yet'
+        self.second_visited = 'not yet'
+
+        def first(evt):
+            assert self.x == 1  # unchanged, sanity check
+            self.x += 1
+            self.eb.dispatch(AnotherEvent('ignored'))
+            assert self.x == 2  # assert that 'second' hasn't been called yet
+            assert self.second_visited == 'not yet'
+            self.first_visited = 'yes'
+            # 'second' should be invoked only after this function returns
+
+        def second(evt):
+            assert self.x == 2
+            assert self.first_visited == 'yes'
+            assert self.second_visited == 'not yet'
+            self.second_visited = 'yes'
+            self.x += 1
+
+        self.eb.register(PingEvent, first)
+        self.eb.register(AnotherEvent, second)
+
+    def test_dispatch(self):
+        self.eb.dispatch(PingEvent('ignored'))
+        assert self.x == 3
+        assert self.first_visited == 'yes'
+        assert self.second_visited == 'yes'
